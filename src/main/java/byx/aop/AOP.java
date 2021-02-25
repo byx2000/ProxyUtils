1d4860baf777307098be1591836b8cdd29e01dc5
byx2000 <416982860@qq.com>
2021-02-25 10:25:55 +0800

    添加cglib动态代理

--- src/main/java/byx/aop/AOP.java
+++ src/main/java/byx/aop/AOP.java
@@ -3,8 +3,12 @@ package byx.aop;
 import byx.aop.core.Invokable;
 import byx.aop.core.MethodInterceptor;
 import byx.aop.core.MethodSignature;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.InvocationHandler;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
 
 /**
@@ -19,16 +23,17 @@ public class AOP
      * @param <T> 返回类型
      * @return 被增强的代理对象
      */
-    @SuppressWarnings("unchecked")
     public static <T> T proxy(Object target, MethodInterceptor interceptor)
     {
-        return (T) Proxy.newProxyInstance(target.getClass().getClassLoader(),
-                target.getClass().getInterfaces(),
-                (proxy, method, args) ->
-                {
-                    Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
-                    return interceptor.intercept(MethodSignature.of(targetMethod), Invokable.of(targetMethod, target), args);
-                });
+        // final类使用jdk代理
+        if (Modifier.isFinal(target.getClass().getModifiers()))
+            return proxyByJdk(target, interceptor);
+        // 没有接口的类使用cglib代理
+        else if (target.getClass().getInterfaces().length == 0)
+            return proxyByCglib(target, interceptor);
+        // 其他情况使用jdk代理
+        else
+            return proxyByJdk(target, interceptor);
     }
 
     /**
@@ -43,5 +48,61 @@ public class AOP
         return type.cast(Proxy.newProxyInstance(type.getClassLoader(),
                 new Class<?>[]{type},
                 (proxy, method, args) -> interceptor.intercept(MethodSignature.of(method), Invokable.of(method, null), args)));
+    }
+
+    /**
+     * 使用jdk动态代理
+     */
+    @SuppressWarnings("unchecked")
+    private static <T> T proxyByJdk(Object target, MethodInterceptor interceptor)
+    {
+        return (T) Proxy.newProxyInstance(target.getClass().getClassLoader(),
+                target.getClass().getInterfaces(),
+                (proxy, method, args) ->
+                {
+                    Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+                    return interceptor.intercept(MethodSignature.of(targetMethod), Invokable.of(targetMethod, target), args);
+                });
+    }
+
+    /**
+     * 使用cglib动态代理
+     */
+    @SuppressWarnings("unchecked")
+    private static <T> T proxyByCglib(Object target, MethodInterceptor interceptor)
+    {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(target.getClass());
+        enhancer.setCallback((InvocationHandler) (proxy, method, args) ->
+                interceptor.intercept(MethodSignature.of(method), Invokable.of(method, target), args));
+        return (T) enhancer.create();
+    }
+
+    /**
+     * 关闭cglib的警告
+     */
+    private static void disableAccessWarnings()
+    {
+        try
+        {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field field = unsafeClass.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            Object unsafe = field.get(null);
+
+            Method putObjectVolatile = unsafeClass.getDeclaredMethod("putObjectVolatile", Object.class, long.class, Object.class);
+            Method staticFieldOffset = unsafeClass.getDeclaredMethod("staticFieldOffset", Field.class);
+
+            Class<?> loggerClass = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field loggerField = loggerClass.getDeclaredField("logger");
+            Long offset = (Long) staticFieldOffset.invoke(unsafe, loggerField);
+            putObjectVolatile.invoke(unsafe, loggerClass, offset, null);
+        }
+        catch (Exception ignored) {}
+    }
+
+    static
+    {
+        disableAccessWarnings();
     }
 }
---
+++ src/test/java/byx/aop/test/CglibTest.java
@@ -0,0 +1,164 @@
+package byx.aop.test;
+
+import byx.aop.core.Invokable;
+import byx.aop.core.MethodInterceptor;
+import byx.aop.core.MethodSignature;
+import org.junit.jupiter.api.Test;
+
+import static byx.aop.AOP.proxy;
+import static byx.aop.core.MethodMatcher.withName;
+import static byx.aop.core.MethodMatcher.withPattern;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CglibTest
+{
+    public static class Student
+    {
+        private int id;
+        private String name;
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+    }
+
+    public interface User
+    {
+        String getUsername();
+        String getPassword();
+    }
+
+    public static final class UserImpl implements User
+    {
+        private String username;
+        private String password;
+
+        public String getUsername()
+        {
+            return username;
+        }
+
+        public void setUsername(String username)
+        {
+            this.username = username;
+        }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public void setPassword(String password)
+        {
+            this.password = password;
+        }
+    }
+
+    @Test
+    public void test1()
+    {
+        int[] id = new int[]{0};
+        String[] name = new String[]{""};
+        boolean[] flag = new boolean[]{false};
+        Student s = proxy(new Student(), (signature, targetMethod, params) ->
+        {
+            flag[0] = true;
+            assertEquals(1, params.length);
+            if (params[0] instanceof Integer)
+                id[0] = (int) params[0];
+            else if (params[0] instanceof String)
+                name[0] = (String) params[0];
+            System.out.println("开始拦截" + signature.getName() + "方法");
+            Object ret = targetMethod.invoke(params);
+            System.out.println("结束拦截" + signature.getName() + "方法");
+            return ret;
+        });
+
+        s.setId(1001);
+        assertTrue(flag[0]);
+        assertEquals(1001, id[0]);
+
+        flag[0] = false;
+        s.setName("XiaoMing");
+        assertTrue(flag[0]);
+        assertEquals("XiaoMing", name[0]);
+    }
+
+    @Test
+    public void test2()
+    {
+        boolean[] flag = new boolean[]{false};
+        Student s = proxy(new Student(), ((MethodInterceptor) (signature, targetMethod, params) ->
+        {
+            flag[0] = true;
+            assertEquals(1, params.length);
+            assertEquals(1001, params[0]);
+            System.out.println("开始拦截" + signature.getName() + "方法");
+            Object ret = targetMethod.invoke(params);
+            System.out.println("结束拦截" + signature.getName() + "方法");
+            return ret;
+        }).when(withName("setId")));
+
+        s.setId(1001);
+        assertTrue(flag[0]);
+
+        flag[0] = false;
+        s.setName("XiaoMing");
+        assertFalse(flag[0]);
+    }
+
+    @Test
+    public void test3()
+    {
+        boolean[] flag = new boolean[]{false};
+        Student s = proxy(new Student(), ((MethodInterceptor) (signature, targetMethod, params) ->
+        {
+            flag[0] = true;
+            System.out.println("开始拦截" + signature.getName() + "方法");
+            Object ret = targetMethod.invoke(params);
+            assertEquals("byx", ret);
+            System.out.println("结束拦截" + signature.getName() + "方法");
+            return "XiaoMing";
+        }).when(withPattern("get(.*)").andReturnType(String.class)));
+
+        s.setName("byx");
+        assertFalse(flag[0]);
+
+        String name = s.getName();
+        assertTrue(flag[0]);
+        assertEquals("XiaoMing", name);
+    }
+
+    @Test
+    public void test4()
+    {
+        boolean[] flag = new boolean[]{false};
+        User user = proxy(new UserImpl(), (signature, targetMethod, params) ->
+        {
+            flag[0] = true;
+            System.out.println("开始拦截" + signature.getName() + "方法");
+            Object ret = targetMethod.invoke(params);
+            System.out.println("结束拦截" + signature.getName() + "方法");
+            return ret;
+        });
+
+        user.getUsername();
+        assertTrue(flag[0]);
+    }
+}
