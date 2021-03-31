c6141a5529e622a9b0443154457c7836c650cd7f
byx2000 <416982860@qq.com>
2021-03-31 10:28:12 +0800

    添加使用示例：模拟Spring的声明式事务

--- src/test/java/byx/util/proxy/test/Example1.java
+++ src/test/java/byx/util/proxy/test/Example1.java
@@ -1,114 +1,159 @@
 package byx.util.proxy.test;
 
-import byx.util.proxy.core.*;
-import byx.util.proxy.exception.TargetMethodException;
+import byx.util.proxy.ProxyUtils;
+import byx.util.proxy.core.MethodInterceptor;
+import byx.util.proxy.core.TargetMethod;
 import org.junit.jupiter.api.Test;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Random;
 
-import static byx.util.proxy.ProxyUtils.*;
+import static byx.util.proxy.core.MethodMatcher.hasAnnotation;
 
 /**
- * 声明式事务管理
+ * 模拟Spring的声明式事务@Transactional
  */
 public class Example1 {
     // 模拟JDBC的Connection
     public static class Connection {
+        private final int id;
+
+        public Connection(int id) {
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
         public void setAutoCommit(boolean flag) {
-            System.out.println("setAutoCommit(" + flag + ")");
-        }
-
-        public void commit() {
-            System.out.println("提交事务");
-        }
-
-        public void rollback() {
-            System.out.println("回滚事务");
-        }
-
-        public void close() {
-            System.out.println("关闭连接");
+            System.out.println(Thread.currentThread().getName() + ": set auto commit " + flag + ": " + id);
         }
 
         public void execute(String sql) {
-            System.out.println("执行sql语句：" + sql);
+            System.out.println(Thread.currentThread().getName() + ": execute '" + sql + "' with connection: " + id);
+        }
+
+        public void commit() {
+            System.out.println(Thread.currentThread().getName() + ": commit: " + id);
+        }
+
+        public void rollback() {
+            System.out.println(Thread.currentThread().getName() + ": rollback: " + id);
+        }
+
+        public void close() {
+            System.out.println(Thread.currentThread().getName() + ": close connection: " + id);
         }
     }
 
-    // 模拟JDBC的DataSource
-    public static class DataSource {
-        public Connection getConnection() {
-            System.out.println("获取连接");
-            return new Connection();
+    // 模拟Spring的JdbcTemplate
+    public static class JdbcTemplate {
+        private static final ThreadLocal<Connection> connHolder = new ThreadLocal<>();
+
+        public static void execute(String sql) {
+            Connection conn = connHolder.get();
+            if (conn == null) {
+                conn = new Connection(new Random().nextInt());
+                System.out.println(Thread.currentThread().getName() + ": create new connection: " + conn.getId());
+                conn.execute(sql);
+                conn.close();
+            } else {
+                conn.execute(sql);
+            }
+        }
+
+        public static void startTransaction() {
+            Connection conn = connHolder.get();
+            if (conn == null) {
+                conn = new Connection(new Random().nextInt());
+                System.out.println(Thread.currentThread().getName() + ": create new connection: " + conn.getId());
+                connHolder.set(conn);
+            }
+            conn.setAutoCommit(false);
+        }
+
+        public static void commitAndClose() {
+            Connection conn = connHolder.get();
+            if (conn != null) {
+                conn.commit();
+                conn.close();
+            }
+            connHolder.remove();
+        }
+
+        public static void rollbackAndClose() {
+            Connection conn = connHolder.get();
+            if (conn != null) {
+                conn.rollback();
+                conn.close();
+            }
+            connHolder.remove();
         }
     }
 
-    // 声明事务管理的注解
+    // 模拟Spring的Transactional注解
     @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.METHOD)
-    @Inherited
-    public @interface Transactional {
-    }
+    public static @interface Transactional {}
 
-    // 模拟连接池
-    private final Connection[] connectionPool = new Connection[1];
-
-    // User服务接口
-    public interface UserService {
-        void insert();
-
-        void delete();
-    }
-
-    // User服务实现类
-    public class UserServiceImpl implements UserService {
-        @Override
-        @Transactional
-        public void insert() {
-            connectionPool[0].execute("INSERT INTO ...");
-        }
-
-        @Override
-        @Transactional
-        public void delete() {
-            connectionPool[0].execute("DELETE FROM ...");
-            throw new RuntimeException("删除时抛出的异常");
-        }
-    }
-
-    // 事务管理器
-    public class TransactionManager implements MethodInterceptor {
-        private final DataSource dataSource;
-
-        public TransactionManager(DataSource dataSource) {
-            this.dataSource = dataSource;
-        }
-
+    // 事务增强拦截器
+    public static class TransactionalInterceptor implements MethodInterceptor {
         @Override
         public Object intercept(TargetMethod targetMethod) {
-            Object[] params = targetMethod.getParams();
+            JdbcTemplate.startTransaction();
             try {
-                connectionPool[0] = dataSource.getConnection();
-                connectionPool[0].setAutoCommit(false);
-                Object ret = targetMethod.invoke(params);
-                connectionPool[0].commit();
-                return ret;
-            } catch (TargetMethodException e) {
-                System.out.println("发生异常：" + e.getMessage());
-                connectionPool[0].rollback();
+                Object obj = targetMethod.invokeWithOriginalParams();
+                JdbcTemplate.commitAndClose();
+                return obj;
+            } catch (Exception e) {
+                JdbcTemplate.rollbackAndClose();
                 return null;
-            } finally {
-                connectionPool[0].close();
             }
+        }
+    }
+
+    public static class UserDao {
+        public void dao1() {
+            JdbcTemplate.execute("sql1");
+        }
+
+        public void dao2() {
+            JdbcTemplate.execute("sql2");
+        }
+    }
+
+    public static class UserService {
+        private final UserDao userDao = new UserDao();
+
+        @Transactional
+        public void service1() {
+            userDao.dao1();
+            userDao.dao2();
+        }
+
+        @Transactional
+        public void service2() {
+            userDao.dao1();
+            int a = 1 / 0;
+            userDao.dao2();
+        }
+
+        public void service3() {
+            userDao.dao1();
+            userDao.dao2();
         }
     }
 
     @Test
     public void test() {
-        UserService userService = proxy(new UserServiceImpl(), new TransactionManager(new DataSource()).when(MethodMatcher.hasAnnotation(Transactional.class)));
+        UserService userService = ProxyUtils.proxy(new UserService(),
+                new TransactionalInterceptor().when(hasAnnotation(Transactional.class)));
 
-        userService.insert();
+        userService.service1();
         System.out.println();
-        userService.delete();
+        userService.service2();
+        System.out.println();
+        userService.service3();
     }
 }
